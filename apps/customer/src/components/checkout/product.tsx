389cfee49afbd11fb4a4fde628d140730a5ed56d
hozayves <muhozayves2014@gmail.com>
2025-02-05 00:58:35 +0200

    Make changes for fonts

--- apps/customer/src/components/checkout/product.tsx
+++ apps/customer/src/components/checkout/product.tsx
@@ -5,8 +5,8 @@ export default function CheckoutProduct() {
         <div className="flex items-center gap-2">
             <Image src="/product/apple.svg" className="w-15 h-15" alt="" width="40" height="40" />
             <div className="flex gap-1 text-base flex-1 items-center">
-                <h3 className="text-lg">Apple</h3>
-                <span className="text-base">x1</span>
+                <h3 className="text-base">Apple</h3>
+                <span className="text-sm">x1</span>
             </div>
             <span className="font-semibold">$80.00</span>
         </div>

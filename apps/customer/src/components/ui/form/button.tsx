ab850d790ade4ca0ef5f2fef89ca71f22e23f9a0
hozayves <muhozayves2014@gmail.com>
2025-02-06 10:12:36 +0200

    Implement reusable button component

---
+++ apps/customer/src/components/ui/form/button.tsx
@@ -0,0 +1,10 @@
+
+
+export default function InputButton() {
+    return (
+        <input
+            type="submit"
+            value="Login"
+            className="px-7 w-full md:px-5 bg-primary text-white rounded-full self-center py-3 text-sm md:mb-0 md:w-full text-center" />
+    )
+}

36427cbf77d4ee2007cbe286ea573dc1cbd9d65c
hozayves <muhozayves2014@gmail.com>
2025-02-07 01:20:46 +0200

    Make a changes so that button could accept icon as optional

--- apps/customer/src/components/ui/form/button.tsx
+++ apps/customer/src/components/ui/form/button.tsx
@@ -1,10 +1,16 @@
+// import Image from "next/image";
 
 
-export default function InputButton() {
+export default function InputButton(
+    { type = "submit", value = "Login", className, icon }:
+        { type?: string, value?: string, className?: string, icon?: JSX.Element }
+) {
     return (
-        <input
-            type="submit"
-            value="Login"
-            className="px-7 w-full md:px-5 bg-primary text-white rounded-full self-center py-3 text-sm md:mb-0 md:w-full text-center" />
+        <div className={`px-7 w-full md:px-5 bg-primary text-white rounded-full self-center py-3 text-sm md:mb-0 md:w-full text-center flex justify-center items-center gap-1 ${className}`}>
+            <input
+                type={type}
+                value={value} />
+            {icon && <span>{icon}</span>}
+        </div>
     )
 }

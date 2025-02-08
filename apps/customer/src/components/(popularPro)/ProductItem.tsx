e42825515b4812bbd60aa576e2cbc4ab0a838f6b
hozayves <muhozayves2014@gmail.com>
2025-02-08 16:41:51 +0200

    Update product items to open product preview when ypu press on eye icon

--- apps/customer/src/components/(popularPro)/ProductItem.tsx
+++ apps/customer/src/components/(popularPro)/ProductItem.tsx
@@ -1,6 +1,10 @@
+'use client'
+
+import { useEffect } from 'react'
 import Image from "next/image"
 import Link from "next/link"
 import Rating from "./Rating"
+import ProductPreview from './productPreview'
 
 interface Product {
     id: number
@@ -14,15 +18,32 @@ interface Product {
 }
 
 interface ProductItemProps {
-    product: Product
+    product: Product,
+    className?: string
 }
 
-function ProductItem({ product }: ProductItemProps) {
+function ProductItem({ product, className }: ProductItemProps) {
+    // Client-side only code: Open the modal when button is clicked
+    const openModal = () => {
+        const modalElement = document.getElementById('my_modal_2') as HTMLDialogElement;
+        if (modalElement) {
+            modalElement.showModal();  // Open the modal
+        }
+    }
+
+    useEffect(() => {
+        // This ensures that the modal is accessible on the client side
+        const modalElement = document.getElementById('my_modal_2');
+        if (modalElement) {
+            // Handle any other client-side logic if needed
+        }
+    }, []);  // Empty dependency array ensures this only runs on mount
+
     return (
-        <div className='border hover:border-primary-hard hover:shadow-[0px_0px_6px_0px_rgba(0,178,7,1)] min-w-[65%] md:min-w-[32%] lg:min-w-[24%] xl:min-w-[19.1%]  md:mb-3 flex items-center p-3 relative transition-all duration-300 cursor-pointer bg-white group rounded-md'>
+        <div className={`border hover:border-primary-hard hover:shadow-[0px_0px_6px_0px_rgba(0,178,7,1)] min-w-[65%] md:min-w-[32%] lg:min-w-[24%] xl:min-w-[19.1%]  md:mb-3 flex items-center p-3 relative transition-all duration-300 cursor-pointer bg-white group rounded-md ${className}`}>
             <div className="flex flex-col gap-3 w-full">
                 <Link href={`/product/${product.name}/${product.id}`}>
-                    <Image src={product.image} alt={product.name} width={200} height={200} className="md:w-40 md:h-40 self-center" />
+                    <Image src={product.image} alt={product.name} width={200} height={200} className="w-full  md:h-40 self-center" />
                 </Link>
                 <div className="flex flex-row justify-between w-full">
                     <div className="flex flex-col gap-1 items-start">
@@ -43,17 +64,30 @@ function ProductItem({ product }: ProductItemProps) {
                 </div>
             </div>
             <div className="flex flex-col gap-2 absolute top-2 right-2 text-gray-800">
-                <button type="button" className={`border rounded-full p-2  cursor-pointer hover:scale-110 transition-all duration-300 ${product.wishList ? 'bg-primary text-white border-primary' : 'border-gray-200 hover:bg-gray-100'} `}>
+                <button type="button" className={`border rounded-full p-2   cursor-pointer hover:scale-110 transition-all duration-300 ${product.wishList ? 'bg-primary text-white border-primary' : 'border-gray-200 hover:bg-gray-100'} `}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                     </svg>
                 </button>
-                <button type="button" className="hover:bg-gray-100 rounded-full p-2 border border-gray-100 bg-white cursor-pointer hover:scale-110 transition-all duration-300">
+                <button
+                    onClick={openModal}
+                    type="button"
+                    className="hover:bg-gray-100 rounded-full p-2 border border-gray-100 bg-white cursor-pointer hover:scale-110 transition-all duration-300">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
                 </button>
+
+                {/* Modal Dialog */}
+                <dialog id="my_modal_2" className="modal px-1 ">
+                    <div className="modal-box w-11/12 max-w-5xl">
+                        <ProductPreview />
+                    </div>
+                    <form method="dialog" className="modal-backdrop">
+                        <button type='button'>close</button>
+                    </form>
+                </dialog>
             </div>
         </div>
     )
